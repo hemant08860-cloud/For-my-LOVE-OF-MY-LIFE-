83db55f1ebeb504f856dddf0362635b81cefc7e2
hemant08860-cloud <hemant.08860@gmail.com>
2026-07-01 22:36:30 +0530

    Update and rename script.js to js/script.js

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const messages = [
+
+    "🌸 Preparing birthday surprises...",
+    "✨ Gathering warm wishes...",
+    "🎨 Decorating memories...",
+    "📚 Packing board luck...",
+    "💖 Almost Ready...",
+    "🫡 Mission Ready!"
+
+];
+
+const bar = document.querySelector(".progress-bar");
+const text = document.getElementById("loadingMessage");
+
+let width = 0;
+let index = 0;
+
+const loading = setInterval(() => {
+
+    width++;
+
+    bar.style.width = width + "%";
+
+    if (width % 20 === 0 && index < messages.length) {
+
+        text.textContent = messages[index];
+        index++;
+
+    }
+
+    if (width >= 100) {
+
+        clearInterval(loading);
+
+        setTimeout(() => {
+
+            document.getElementById("loader").style.opacity = "0";
+
+            setTimeout(() => {
+
+                document.getElementById("loader").style.display = "none";
+
+                document.getElementById("hero").style.display = "flex";
+
+                document.body.style.overflow = "auto";
+
+            }, 800);
+
+        }, 500);
+
+    }
+
+}, 40);
